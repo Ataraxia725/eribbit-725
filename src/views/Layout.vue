19b2482db672d58d3073e2e438f428492ab26ddf
Ataraxia725 <sanciyuanzbg@gmail.com>
2022-05-14 01:04:07 +0800

    头部导航组建

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,21 +1,26 @@
 <template>
   <AppTopnav/>
-  <header>头部</header>
-  <main>
+  <AppHeader/>
+  <main class="app-body">
+    // 二级路由
     <router-view></router-view>
   </main>
-  <footer>底部</footer>
-
+  <AppFooter/>
 </template>
 
 <script>
 import AppTopnav from '@/components/app-topnav'
+import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
+
 export default {
   name: 'xtx-layout',
-  components: { AppTopnav }
+  components: { AppTopnav, AppHeader, AppFooter }
 }
 </script>
 
 <style scoped lang="less">
-
+.app-body{
+  min-height: 600px;
+}
 </style>
