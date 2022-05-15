e45643d429a04dd218e7dc6a483c8ce85c0776b5
Ataraxia725 <sanciyuanzbg@gmail.com>
2022-05-15 20:11:35 +0800

    头部分类导航渲染

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,21 +1,30 @@
 <template>
   <AppTopnav/>
-  <header>头部</header>
-  <main>
+  <AppHeader/>
+  <main class="app-body">
+    // 二级路由
     <router-view></router-view>
   </main>
-  <footer>底部</footer>
-
+  <AppFooter/>
 </template>
 
 <script>
 import AppTopnav from '@/components/app-topnav'
+import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
+import { useStore } from 'vuex'
 export default {
   name: 'xtx-layout',
-  components: { AppTopnav }
+  components: { AppTopnav, AppHeader, AppFooter },
+  setup () {
+    const store = useStore()
+    store.dispatch('category/getList')
+  }
 }
 </script>
 
 <style scoped lang="less">
-
+.app-body{
+  min-height: 600px;
+}
 </style>
